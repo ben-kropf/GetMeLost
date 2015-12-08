147a9ad47d4ed997a1417f67a8659eb8f8fdbdc5
Ben Kropf <benkropf@live.com>
2015-12-08 10:24:09 -0500

    Added maps

--- Get Me Lost/TakeHomeViewController.swift
+++ Get Me Lost/TakeHomeViewController.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class TakeHomeViewController: UIViewController {
 
+    let regionRadius: CLLocationDistance = 1000
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 38.981765, longitude: -76.937507)
+        centerMapOnLocation(initialLocation)
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
